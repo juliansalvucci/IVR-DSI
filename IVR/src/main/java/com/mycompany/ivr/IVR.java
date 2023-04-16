55810ac274eb7d15dc616219f1ed3754fed207ec
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2023-04-16 00:49:17 -0300

    proyecto

---
+++ IVR/src/main/java/com/mycompany/ivr/IVR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ivr;
+
+/**
+ *
+ * @author jlssa
+ */
+public class IVR {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+    }
+}
