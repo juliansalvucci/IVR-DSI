19ec5397c1d6c935a25d09e510ede367feef1342
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2023-06-17 18:39:41 -0300

    csv v1.0

---
+++ IVR/src/test/java/Test.java
@@ -0,0 +1,100 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.mycompany.ivr.Clases.Encuesta;
+import com.mycompany.ivr.Clases.Llamada;
+import com.opencsv.CSVWriter;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("¡Hola TEST!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence-unit");
+        EntityManager em = emf.createEntityManager();
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Llamada> cq = cb.createQuery(Llamada.class);
+        Root<Llamada> root = cq.from(Llamada.class);
+
+        cq.select(root); // Obtener llamadas desde base de datos.
+
+        TypedQuery<Llamada> query = em.createQuery(cq);
+        List<Llamada> llamadas = query.getResultList();
+
+        System.out.println("LLAMADASSSSSSSSSSSSSSSSS" + llamadas.size());
+
+        Llamada test = llamadas.get(0);
+
+        List<String> respuestas = test.getRespuestas();
+
+        System.out.println("RESPUESTAS" + respuestas);
+
+        String respuesta = respuestas.get(0); // Toma una respuesta cualquiera, en este caso, la primera.
+        String[] partes = respuesta.split("_"); // Divide la cadena a partir del caracter "_".
+        String respuestaPosible = partes[1];
+
+        System.out.println("POSIBLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE" + respuestaPosible);
+
+        CriteriaBuilder cb2 = em.getCriteriaBuilder();
+        CriteriaQuery<Encuesta> cq2 = cb2.createQuery(Encuesta.class);
+        Root<Encuesta> root2 = cq2.from(Encuesta.class);
+
+        cq2.select(root2); // Se levantan en memoria todas las encuestas.
+
+        TypedQuery<Encuesta> query2 = em.createQuery(cq2);
+        List<Encuesta> encuestas = query2.getResultList();
+
+        for (Encuesta encuesta : encuestas) { // Recorrer cada encuesta.
+            Boolean esEncuesta = encuesta.esEncuestaDeCliente(respuestaPosible); // Verificar si es encuesta de cliente,
+                                                                                 // a partir del espacio en memoria de
+                                                                                 // respuesta posible.
+            if (esEncuesta) { // Si es encuesta de cliente.
+                System.out.println("DESCRIPCION" + encuesta.getDescripcionEncuesta());
+            } else {
+                System.out.println("PREGUNTASSSSSSSS" + encuesta.getPreguntas().size());
+            }
+
+        }
+
+        try {
+            String csvFile = "C:\\Users\\jlssa\\Documents\\archivo.csv";
+            FileWriter writer = new FileWriter(csvFile);
+            CSVWriter csvWriter = new CSVWriter(writer);
+
+            String[] encabezado = { "Nombre", "Apellido", "Edad" };
+            csvWriter.writeNext(encabezado);
+
+            ArrayList<String> datos = new ArrayList<>();
+
+            System.out.println("¿CUANTAS HAY?" + respuestas.size());
+
+            for (int i = 0; i < respuestas.size(); i++) {
+
+                String rta = respuestas.get(i); // Toma una respuesta cualquiera, en este caso, la primera.
+                String[] parte = rta.split("_"); // Divide la cadena a partir del caracter "_".
+                String resp = "¿PREGUNTA DE PRUEBA PARA PROBAR?" + " | " + parte[0];
+
+                datos.add(resp);
+            }
+
+            for (String dato : datos) {
+                csvWriter.writeNext(new String[] { dato });
+            }
+
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
