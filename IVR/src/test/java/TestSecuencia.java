a492c3fdd56196158a4805fdb91956a7442b4c3e
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2023-06-18 12:54:15 -0300

    test secuencia v1.5

--- IVR/src/test/java/TestSecuencia.java
+++ IVR/src/test/java/TestSecuencia.java
@@ -1,6 +1,7 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -11,7 +12,6 @@ import javax.persistence.Persistence;
 import com.mycompany.ivr.Clases.Llamada;
 import com.mycompany.ivr.Controlador.ControladorConsultarEncuesta;
 
-
 public class TestSecuencia {
     public static void main(String[] args) {
         System.out.println("¡Hola Secuencia!");
@@ -21,45 +21,78 @@ public class TestSecuencia {
 
         ControladorConsultarEncuesta gestor = new ControladorConsultarEncuesta(em);
 
+        Scanner scannerFechaInicio = new Scanner(System.in);
+        System.out.print("Ingresa una fecha (formato: yyyy-MM-dd): ");
+        String fechaInicioIngresada = scannerFechaInicio.nextLine();
+        //scannerFechaInicio.close();
 
-        LocalDate localDate = LocalDate.of(2021, 6, 3);
-        Instant instant = localDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate fechaInicio = LocalDate.parse(fechaInicioIngresada, formatter);
+
+        Instant instant = fechaInicio.atStartOfDay(ZoneId.systemDefault()).toInstant();
         Date date = Date.from(instant);
-        System.out.println(date);
+        System.out.println("La fecha ingresada es: " + fechaInicio);
 
-        LocalDate localDate1 = LocalDate.of(2023, 10, 3);
-        Instant instant1 = localDate1.atStartOfDay(ZoneId.systemDefault()).toInstant();
-        Date date1 = Date.from(instant1);
-        System.out.println(date1);
+        /* FECHA FIN */
 
-        gestor.tomarPeriodo(date, date1);
+        //Scanner scannerFechaInicio = new Scanner(System.in);
+        System.out.print("Ingresa una fecha (formato: yyyy-MM-dd): ");
+
+        String fechaFinIngresada = scannerFechaInicio.nextLine();
+
+        DateTimeFormatter formatterFechaFin = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate fechaFin = LocalDate.parse(fechaFinIngresada, formatterFechaFin);
+
+        Instant instantFin = fechaFin.atStartOfDay(ZoneId.systemDefault()).toInstant();
+        Date dateFin = Date.from(instantFin);
+        System.out.println("La fecha ingresada es: " + fechaFin);
+
+        // Cerrar el objeto Scanner después de su uso
+
+        /*
+         * LocalDate localDate = LocalDate.of(2021, 6, 3);
+         * Instant instant = localDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
+         * Date date = Date.from(instant);
+         * System.out.println(date);
+         */
+
+        /*
+         * LocalDate localDate1 = LocalDate.of(2023, 10, 3);
+         * Instant instant1 =
+         * localDate1.atStartOfDay(ZoneId.systemDefault()).toInstant();
+         * Date date1 = Date.from(instant1);
+         * System.out.println(date1);
+         */
+
+        gestor.tomarPeriodo(date, dateFin);
 
         System.out.println(gestor.getFechaInicio() + "" + gestor.getFechaFin());
 
         gestor.buscarLlamadasConEncuesta();
 
-        if(gestor.getListaLlamadas().size() > 0){
+        if (gestor.getListaLlamadas().size() > 0) {
             System.out.println("CANTIDAD LLAMADAS ENCONTRADAS" + gestor.getListaLlamadas().size());
-        }else{
+        } else {
             System.out.println("NO HAY LLAMADAS EN EL PERIÓDO ESTABLECIDO");
             System.exit(0);
         }
-        
-        Scanner scanner = new Scanner(System.in);
+
+        //Scanner scannerNumeroLlamada = new Scanner(System.in);
 
         System.out.print("Ingresa un número: ");
         // Leer el número ingresado por el usuario
-        int numero = scanner.nextInt();
+        int numero = scannerFechaInicio.nextInt();
 
         Llamada llamadaDePrueba = gestor.getListaLlamadas().get(numero);
 
-        scanner.close();
+        scannerFechaInicio.close();
 
         gestor.tomarSeleccionLlamadaConEncuesta(llamadaDePrueba);
 
         gestor.obtenerDatosLlamada();
 
-        System.out.println("DATOS LLAMADA" + gestor.getNombreCliente() + gestor.getUltimoEstadoLlamada() + gestor.getDuracionLlamada());
+        System.out.println("DATOS LLAMADA" + gestor.getNombreCliente() + gestor.getUltimoEstadoLlamada()
+                + gestor.getDuracionLlamada());
 
         gestor.obtenerDatosEncuesta();
 
